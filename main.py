e91a7160cacf046fbbb7d0965684cb0a452bfb97
DESKTOP-CTVKBG2\mifay <pmtl.stl@gmail.com>
2023-03-22 22:58:16 -0400

    newer user agent

--- main.py
+++ main.py
@@ -518,7 +518,7 @@ class PlaceClient:
                             client.proxies = proxy.get_random_proxy(self, name)
                             client.headers.update(
                                 {
-                                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36"
+                                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
                                 }
                             )
 
